a528668b86bdd9d04a85d7d07494a60a013a493e
tricyzhou <tricyzhou@tencent.com>
2020-09-28 19:19:27 +0800

    test case

--- js2kv_test.go
+++ js2kv_test.go
@@ -3,33 +3,40 @@ package gojson
 import "testing"
 
 func Test_Js2kv_1(t *testing.T){
-	data := "{\"k1\":{\"age\":25,\"name\":\"tricy\",\"education\":{\"bachelor\":\"CUMTB\",\"master\":\"BUAA\"}}," +
-		"\"k2\":null,\"k3\":true,\"k4\":[1,2,3],\"k5\":1.2,\"k6\":\"abc\"}"
-	kv, err := Js2kv(data, "-")
-	if err != nil{
-		t.Error("Js2kvc测试出错")
-	}else{
-		if kv != "{\"k1-age\":25,\"k1-education-bachelor\":\"CUMTB\",\"k1-education-master\":\"BUAA\"," +
-			"\"k1-name\":\"tricy\",\"k2\":null,\"k3\":true,\"k4\":[1,2,3],\"k5\":1.2,\"k6\":\"abc\"}"{
-			t.Error("Js2kvc测试出错")
-		}else{
-			t.Log("Js2kvc测试通过")
+
+	var tests = []struct{
+		input string
+		want string
+		sep string
+	}{
+		{"{\"k1\":{\"age\":25,\"name\":\"tricy\",\"education\":{\"bachelor\":\"CUMTB\",\"master\":\"BUAA\"}}," +
+			"\"k2\":null,\"k3\":true,\"k4\":[1,2,3],\"k5\":1.2,\"k6\":\"abc\"}",
+			"{\"k1-age\":25,\"k1-education-bachelor\":\"CUMTB\",\"k1-education-master\":\"BUAA\"," +
+			"\"k1-name\":\"tricy\",\"k2\":null,\"k3\":true,\"k4\":[1,2,3],\"k5\":1.2,\"k6\":\"abc\"}",
+			"-"},
+	}
+	for _, test := range tests{
+		if got, _ := Js2kv(test.input, test.sep);got!=test.want{
+			t.Errorf("Js2kv(%v)=%v", test.input, got)
 		}
 	}
 }
 
 func Test_Kv2js_1(t *testing.T){
-	data := "{\"k1-age\":25,\"k1-name\":\"tricy\",\"k1-education-bachelor\":\"CUMTB\",\"k1-education-master\":\"BUAA\"," +
-		"\"k2\":null,\"k3\":true,\"k4\":[1,2,3],\"k5\":1.2,\"k6\":\"abc\"}"
-	js, err := Kv2js(data, "-")
-	if err != nil{
-		t.Error("Kv2js测试出错")
-	}else{
-		if js != "{\"k1\":{\"age\":25,\"education\":{\"bachelor\":\"CUMTB\",\"master\":\"BUAA\"},\"name\":\"tricy\"}," +
-		"\"k2\":null,\"k3\":true,\"k4\":[1,2,3],\"k5\":1.2,\"k6\":\"abc\"}"{
-			t.Error("Kv2js测试出错")
-		}else{
-			t.Log("Kv2js测试通过")
+	var tests = []struct{
+		input string
+		want string
+		sep string
+	}{
+		{	"{\"k1-age\":25,\"k1-education-bachelor\":\"CUMTB\",\"k1-education-master\":\"BUAA\"," +
+			"\"k1-name\":\"tricy\",\"k2\":null,\"k3\":true,\"k4\":[1,2,3],\"k5\":1.2,\"k6\":\"abc\"}",
+			"{\"k1\":{\"age\":25,\"education\":{\"bachelor\":\"CUMTB\",\"master\":\"BUAA\"}," +
+			"\"name\":\"tricy\"},\"k2\":null,\"k3\":true,\"k4\":[1,2,3],\"k5\":1.2,\"k6\":\"abc\"}",
+			"-"},
+	}
+	for _, test := range tests{
+		if got, _ := Kv2js(test.input, test.sep);got!=test.want{
+			t.Errorf("Kv2js(%v)=%v", test.input, got)
 		}
 	}
 }
